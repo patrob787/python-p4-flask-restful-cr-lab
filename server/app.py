6dd74054808ad14743c971b32cc1379b4537bdb4
Patrick Roberts <patrob22@gmail.com>
2023-05-24 15:28:52 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db, Plant
 
@@ -11,16 +12,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
 
+CORS(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        resp = make_response(plants_list, 200)
+        return resp
+    
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+
+        resp = make_response(new_plant_dict, 201)
+        return resp
+    
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        resp = make_response(plant_dict, 200)
+        return resp
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
